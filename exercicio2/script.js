f87adba1c0f54ea3c07d6eaec669a03e860b25cb
camilanit <professoracamilarj@gmail.com>
2022-07-26 18:37:41 -0300

    Caracteres

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,8 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const minhaStringTrim = minhaString.trim()
+
+console(minhaStringTrim)
+console.log(`Sem o comando Trim há $`{minhaString.length} carateres \nCom o comando Trim a variavel fica com ${minhaStringTrim.lenght} carateres`)
+
+const textoSubstituido = minhaString.replace(`________.`,`sticioso`)
+console. log(textoSubstituido)
