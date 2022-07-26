7b72ed91af08834ce8699466499dcf02570f50c6
Hugowave88 <hugohedgehog24@gmail.com>
2022-07-26 18:57:08 -0300

    TesteString

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,5 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const templeString = "Eu não sou superticioso, mas sou um pouco"
+console.log(trim(), templeString)
+//const templeString = "A cor favorita de ${nome} é ${cor}"
+//console.log(templeString)
