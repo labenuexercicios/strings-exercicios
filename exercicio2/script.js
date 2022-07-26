a3d579e7757ecd5569c82488cf31f924668ca938
hilariohabhab <hilariohabhab@hotmail.com>
2022-07-25 23:31:06 -0300

    Exercicio finalizado

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,5 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const novaString = minhaString.trim()
+const stringReplace = novaString.replace("________", "sticioso");
+
+console.log(stringReplace)
