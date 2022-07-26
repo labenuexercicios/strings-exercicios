bb88291ea58b183c95b4009183da25993ddb9108
JulianDiel <drdiel@hotmail.com>
2022-07-26 20:14:13 -0300

    atualização de exercicios

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,10 +1,7 @@
 //Crie a const para a frase aqui
 const frase = `Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"`;
-const trocaString1 = `${frase.replaceAll("verde","rosa")}`
-const trocaString2 = `${frase.replaceAll("azul","laranja")}`
+const trocaString1 = `${frase.replaceAll("verde","rosa").replaceAll("azul","laranja").replace("mas não deixe o gato sair","MAS NÃO DEIXE O GATO SAIR")}`//replace em cima de replace, funciona com toUpperCase etc...
 
-console.log(trocaString1);
-console.log(trocaString2);
-console.log(trocaString2.includes("verde"));
-console.log(trocaString2.includes("laranja"));
-console.log(frase.replaceAll("mas não deixe o gato sair","MAS NÃO DEIXE O GATO SAIR"));
+console.log(frase);// original
+console.log(trocaString1);//modificado
+
