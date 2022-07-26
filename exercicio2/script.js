432b5217ceb2bbfb692252978b891bf500cddf97
LABNUISA <araujo_isaias@outlook.com>
2022-07-26 17:59:59 -0300

    atividade

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,9 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      .";
+console.log(minhaString.length)
+console.log(minhaString.trim())
+
+
+const novaString = minhaString.replace("________", "sticioso")
+console.log(novaString)
+
