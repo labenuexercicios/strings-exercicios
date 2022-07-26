d0e4da7df7da5e98ba66f7059d67653e23114541
Jessica Faustino <jessicacfaustino@gmail.com>
2022-07-26 11:47:53 -0300

    Exercício finalizado

---
+++ exercicio1/script.js
@@ -0,0 +1,11 @@
+const nome = prompt ("Qual seu nome?");
+const comida1= prompt ("Qual sua comida favorita?");
+const comida2= prompt ("Qual sua comida favorita 2?");
+const comida3= prompt ("Qual sua comida favorita 3?");
+
+
+
+console.log (`Estas são as comidas favortitas de ${nome}
+- ${comida1}
+- ${comida2}
+- ${comida3}`);
