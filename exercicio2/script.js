dc6f1a0502b97558bc78a1e81d79a1804864d654
Nhaykwollfer <108698094+Nhaykwollfer@users.noreply.github.com>
2022-07-26 16:11:24 -0300

    exercicio concluido

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,11 @@
+//Exercicio 2
+
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+console.log(minhaString.length);
+console.log(minhaString.trim());
+
+const minhaString1 = minhaString.trim();
+console.log(minhaString1.length);
+
+const minhaString2 = minhaString.replace("________", "sticioso");
+console.log(minhaString2);
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,12 @@
 //Crie a const para a frase aqui
+
+const frase = 'Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"'
+console.log(frase);
+
+const frase1 = frase.replaceAll("verde", "rosa");
+const frase2 = frase1.replaceAll("azul", "laranja");
+console.log(frase2);
+
+const frase3 = frase2.replaceAll("mas não deixe o gato sair", "MAS NÃO DEIXE O GATO SAIR");
+console.log(frase3);
+
