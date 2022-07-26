467e43eac647450e427f152616f140190e608191
Thaila Marani <thaila_marani@hotmail.com>
2022-07-26 16:07:18 -0300

    Entrega dos exercícios 1, 2 e 3

---
+++ exercicio1/script.js
@@ -0,0 +1,11 @@
+// PARTE 1
+let nome = prompt('Digite seu nome');
+let comida1 = prompt ('Digite sua comida favorita');
+let comida2 = prompt ('Digita sua segunda comida favorita');
+let comida3 = prompt ('Digite sua terceira comida favorita');
+
+// PARTE 2
+console.log(`Estas são as comidas favoritas de ${nome} \n 
+Comida1: ${comida1} \n
+Comida2: ${comida2} \n
+Comida3: ${comida3}`);
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,14 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+console.log(`Primeira frase: ${minhaString}`)
+
+// PARTE 1
+console.log(`Segunda frase: ${minhaString.trim()}`);
+
+// PARTE 2
+minhaString2 = minhaString.trim()
+console.log(`Quantidade de caracteres na primeira frase ${minhaString.length}`);
+console.log (`Quantidade de caracteres na segunda frase ${minhaString2.length}`)
+
+// PARTE 3
+let minhaString3 = minhaString2.replace("________" , "sticioso")
+console.log(minhaString3)
