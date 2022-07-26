d20afda166e9e09f760445c0de8d8ac451ddb79d
YOUR NAME <YOUR@EMAIL.com>
2022-07-26 18:14:05 -0300

    exercicio-feito

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+const user = prompt("Qual seu nome?")
+const comidaFav1 = prompt("Qual a sua comida favorita 1?")
+const comidaFav2 = prompt("Qual a sua comida favorita 2?")
+const comidaFav3 = prompt("Qual a sua comida favorita 3?")
+
+console.log(Essas são as comidas favoritas do ${user} \n${nomeComida1} \n${nomeComida2} \n${nomeComida3}) 
