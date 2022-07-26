8fd8f9caef0542e0f56ad17b7eb1ac88384e61bb
YOUR NAME <YOUR@EMAIL.com>
2022-07-26 20:53:27 -0300

    excercicio-feito

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -2,5 +2,4 @@ const user = prompt("Qual seu nome?")
 const comidaFav1 = prompt("Qual a sua comida favorita 1?")
 const comidaFav2 = prompt("Qual a sua comida favorita 2?")
 const comidaFav3 = prompt("Qual a sua comida favorita 3?")
-
-console.log(Essas são as comidas favoritas do ${user} \n${nomeComida1} \n${nomeComida2} \n${nomeComida3}) 
+console.log(`Estas são as comidas favoritas de ${nomeDoUsuario} \n- ${comida1} \n- ${comida2} \n- ${comida3} `);
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,10 @@
 //Crie a const para a frase aqui
+//a) Crie uma `const` no seu código para guardar a frase (com aspas e tudo);
+const frase = 'Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"'
+//b) Crie uma nova string a partir da primeira, trocando **verde** por **rosa**, e **azul** por **laranja**;
+const frase1 = frase.replace
+
+
+//c) Verifique se a nova string inclui **verde**, e se inclui **laranja**.
+
+//**EXTRA:** tente fazer o “mas não deixe o gato sair” ficar em maiúsculo, assim como o “BOAS VINDAS”
