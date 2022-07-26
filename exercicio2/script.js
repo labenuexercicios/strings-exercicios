461d5a40dd098e2b3d987dafe5972447a55a09cc
leoosouza <leonardo.contatosouza@gmail.com>
2022-07-26 18:25:37 -0300

    conclusao-ex-009

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,12 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+let minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+
+
+stringSemTrim = minhaString.trim()
+console.log(stringSemTrim.length);
+
+console.log(minhaString.length);
+console.log(minhaString);
+
+let novaString = minhaString.replaceAll('________', 'sticioso')
+console.log(novaString);
