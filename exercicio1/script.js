3809d8f290fa40283cb3ab1c134af4c85e65de66
CamilaKFranco <koalakirk007@gmail.com>
2022-07-26 17:46:13 -0300

    Turma D Camila Franco

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+const nome = prompt('Qual seu nome?')
+const comida1 = prompt('Qual suas Tres comidas favoritas?')
+const comida2 = prompt('Qual suas Tres comidas favoritas?')
+const comida3 = prompt('Qual suas Tres comidas favoritas?')
+
+console.log (`Estas sao as comidas favoritas de ${nome} ${comida1} , ${comida2} e ${comida3}`)
