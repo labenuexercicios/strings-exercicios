86ea91ad0d01e4bbc250086452cdf6f7f8e92a52
LABNUISA <araujo_isaias@outlook.com>
2022-07-26 18:58:19 -0300

    atualização

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,13 @@
-//Crie a const para a frase aqui
+const nova = "Jorge tem uma casa verde e com portão azul, com os dizeres: BOAS VINDAS, mas não deixe o gato sair"
+console.log(nova)
+
+//não consegui de jeito algum incluir as aspas
+
+const novaString = (nova.replace("verde","rosa"))
+console.log(novaString)
+
+const novaString1 = (novaString.replace("azul","laranja"))
+console.log(novaString1)
+
+//const novaString2 = novaString1.toLowerCase()
+
