c74f43bf7fc3766bece50139a0fe8da954e0e2a7
Stefanie Duwe <fafidw@gmail.com>
2022-07-26 18:01:09 -0300

    exercício concluído

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+const nome = prompt("Qual seu nome?")
+const comida1 = prompt("Qual sua comidas favorita?")
+const comida2 = prompt("Qual sua 2ª comida favorita?")
+const comida3 = prompt("Qual sua 3ª comida favorita?")
+
+
+const frase = "Estas são as comidas favoritas de " +nome + "\n" +comida1+ "\n" +comida2+ "\n" +comida3
+
+console.log(frase)
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,9 @@
 //Crie a const para a frase aqui
+const frase = `Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"`
+
+const substituirVerde = frase.replace("verde", "rosa")
+const substituirAzul = substituirVerde.replace("azul", "laranja")
+
+
+console.log(substituirAzul)
+
