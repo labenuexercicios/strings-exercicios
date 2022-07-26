d1bc98ba52aeaf940daf53f0d40448dd57b914e6
Gvitor10 <gvitor7062@gmail.com>
2022-07-26 20:59:35 -0300

    Finalizado os exercicios 1,2 e 3

---
+++ exercicio1/script.js
@@ -0,0 +1,10 @@
+// variaveis
+
+let nome = prompt('Qual o seu nome?');
+let comida01 = prompt('Digite o seu pra to favorito!');
+let comida02 = prompt('Digite outro prato favorito!');
+let comida03 = prompt('Só mais outro prato favorito!');
+
+// imprimindo as comidas
+
+console.log(` Esta sao as comidas favoritas de ${nome}:\n ${comida01}\n ${comida02}\n ${comida03}`);
