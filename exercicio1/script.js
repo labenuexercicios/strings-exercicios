9347a760c5c8610ec422372d8ed14b76ca8420af
Emerson-M-de-Lira <emerson.lira.monteiro@gmail.com>
2022-07-26 22:17:36 -0300

    exercicios-009

---
+++ exercicio1/script.js
@@ -0,0 +1,10 @@
+//--------
+//--------
+
+const nomeDoUsuario = prompt("Qual é seu nome?");
+
+const comida1 = prompt("Qual é a sua comida favorita?");
+const comida2 = prompt("Qual é a sua comida favorita?");
+const comida3 = prompt("Qual é a sua comida favorita?");
+
+console.log (`Estas são as comidas favoritas de ${nomeDoUsuario} \n ${comida1} \n ${comida2} \n ${comida3}`);
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,13 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+
+let minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+console.log(minhaString)
+console.log(minhaString.length);
+
+minhaString = minhaString.trim()
+console.log(minhaString.length)
+const minhaString1 = minhaString.trim();
+console.log(minhaString1.length);
+
+const novaMinhaString = minhaString1.replace("________","sticioso")
+console.log(novaMinhaString)
