e7c1a5488ecd083567b44a6b8ccaf48bec0c275f
Gustavo Xavier <gustavoxr26@gmail.com>
2022-07-27 17:38:32 -0300

    Atividade 009

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,7 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const novaFrase = minhaString.replace("________", "sticioso")
+
+minhaStringNova = minhaString.trim()
+console.log(minhaStringNova)
+console.log(`Quantidade de caracteres: \nAntes = ${minhaString.length} \nDepois = ${minhaStringNova.length}`)
+console.log(novaFrase)
