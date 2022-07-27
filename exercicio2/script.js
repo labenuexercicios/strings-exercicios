4a9567dc5e856fa5d3847998684795d0a67e003c
anapgouveia <anapgouveia97@gmail.com>
2022-07-26 21:01:46 -0300

    atividade realizada

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,7 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      " 
+
+console.log(minhaString)
+console.log(minhaString.trim())
+console.log(minhaString.length)
+console.log(minhaString.replaceAll("________", "sticioso"))
