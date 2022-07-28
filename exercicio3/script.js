6b592410874eb5982acc4704ffed920696a76dc4
MathMendesReis <matheusmendesreis@outlook.com>
2022-07-28 18:46:22 -0300

    Exercicios resolvidos

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,7 @@
 //Crie a const para a frase aqui
+const frase = [`Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\"`];
+console.log(frase);
+
+/
+
+console.log(frase,length)
