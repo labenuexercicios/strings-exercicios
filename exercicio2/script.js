4194f7da31105b913e0d6ee3cb3d4a454ac272d4
karineveppo <karineveppo83@gmail.com>
2022-08-31 18:23:09 -0300

    exercicios aula009.

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,6 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+console.log(minhaString)
+console.log(`A string antes da remoção dos espaços tem ${minhaString.length} caracteres.`)
+console.log( `A string depois de remover os espaços ficou com ${minhaString.trim().length} caracteres.`)
+console.log(minhaString.replace("________.","sticioso"))
