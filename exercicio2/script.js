9171cfd3dee922776d880b2cbfbf557c96c361ac
PYTERODACTYL <pyter512@live.com>
2022-08-31 16:38:41 -0300

    feito

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,6 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const stringTrimada = minhaString.trim()
+const stringReformada = minhaString.replace("________","sticioso")
+console.log(minhaString.length)
+console.log(stringTrimada.length)
+console.log(stringReformada)
