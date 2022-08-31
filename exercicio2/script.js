320a3a8c9646cc965fafaf304238584d1171852c
endioliveira <eomedeiros21@gmail.com>
2022-08-31 13:01:20 -0300

    métodos de utilização e propriedades de string

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,9 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const novaFrase = minhaString.trim()
+
+console.log("Frase sem o excesso de espaços no final: ", novaFrase)
+console.log("Quantidade de caracteres da primeira frase: ", minhaString.length)
+console.log("Quantidade de caracteres ca segunda frase: ", novaFrase.length)
+
+const novaFrase2 = minhaString.replace("________", "sticioso")
+console.log(novaFrase2)
