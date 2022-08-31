799fd801fd919a0bc2ed6798077000b05cb4f967
VictorOgataj8 <victor.ogataj8@gmail.com>
2022-08-31 13:04:23 -0300

    feito o exercicio 1

---
+++ exercicio1/script.js
@@ -0,0 +1,10 @@
+let nome= prompt("Qual e o seu nome?")
+const comida1=prompt("Insira tres comidas favoritas")
+const comida2=prompt()
+const comida3=prompt()
+
+console.log(`Essas sao comidas favoritas do ${nome}
+1-${comida1}
+2-${comida2}
+3-${comida3}
+`)
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,8 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+let minhaString="Eu não sou supersticioso, mas sou um pouco ________.      "
+// console.log(`antes:${minhaString.length} 
+// tirar os espacos:${minhaString.trim()} 
+// depois:${minhaString.length}
+// `)
+
+console.log(minhaString.trim(), minhaString.length)
+
