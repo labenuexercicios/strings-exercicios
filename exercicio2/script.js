3b7838b97526115e3487f1a90528f3714a51e879
glauciasonda <glauciasonda@gmail.com>
2022-08-31 16:11:34 -0300

    Ëxercícios Aula 009

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,9 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+// Exercício 2
+
+let minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+let tamanhoString = minhaString.length
+console.log(`O tamanho da string com espaços é: ${tamanhoString}`)
+tamanhoString = minhaString.trim().length
+console.log(`O tamanho da string sem espaços é: ${tamanhoString}`)
+minhaString = minhaString.replace("________", "sticioso")
+console.log(minhaString)
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,25 @@
-//Crie a const para a frase aqui
+// Exercício 3
+
+//a)
+const frase = `Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"`
+console.log(frase)
+//b)
+const fraseAlterada = frase.replace("verde","rosa").replace("azul","laranja")
+console.log(fraseAlterada)
+//c)
+let possuiVerde = fraseAlterada.includes("verde")
+let possuiAzul = fraseAlterada.includes("azul")
+console.log(`possui a cor verde? ${possuiVerde} \n possui a cor azul? ${possuiAzul}  `)
+
+/**Extra 1 */ 
+let fraseExtra = frase.replace("mas não deixe o gato sair", "MAS NÃO DEIXE O GATO SAIR")  
+console.log(fraseExtra) 
+/* Extra 2*/
+/*let posicaoInicial = frase.indexOf("mas")
+let posicaoFinal = frase.indexOf("sair")
+//fraseExtra = frase.substring((0,94))
+console.log(fraseExtra)
+console.log(posicaoInicial)
+console.log(posicaoFinal) 
+
+//**EXTRA:** tente fazer o “mas não deixe o gato sair” ficar em maiúsculo, assim como o “BOAS VINDAS” */
