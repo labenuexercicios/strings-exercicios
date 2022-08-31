cbf4a4878ce61844baccad10ae5409311578af37
endioliveira <eomedeiros21@gmail.com>
2022-08-31 15:55:53 -0300

    alterando exibição de strings, e mais métodos

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,17 @@
 //Crie a const para a frase aqui
+
+const frase = 'Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair"\.'
+const fraseNova = frase.replace("verde", "rosa").replace("azul", "laranja")
+
+console.log(frase)
+console.log(fraseNova)
+// console.log(`Possui a cor verde e laranja? ${fraseNova.includes("verde") && fraseNova.includes("laranja")}`) //false
+
+let possuiVerde = fraseNova.includes("verde")
+let possuiLaranja = fraseNova.includes("laranja")
+console.log("Possui a cor verde?", possuiVerde)
+console.log("Possui a cor laranja?", possuiLaranja)
+
+const fraseNova2 = "mas não deixe o gato sair"
+console.log(`Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, ${fraseNova2.toUpperCase()}\"`)
+
