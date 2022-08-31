820630e6a2677e00a6515a8f0ba9ea814928714c
atilabarbosa <atilacabo@gmail.com>
2022-08-31 15:43:48 -0300

    exercício 3108

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,11 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+//Remova o excesso de espaços no final da string
+let stringOrganizada = minhaString.trim()
+
+//exiba, em um console.log() a quantidade de caracteres da string, antes e depois da remoção dos espaços
+console.log("A primeira string tem " + minhaString.length + " caracteres e a segunda tem " + stringOrganizada.length)
+
+//Substitua os traços `________` por “sticioso”
+let subst = stringOrganizada.replace("________","sticioso")
+console.log(subst)
