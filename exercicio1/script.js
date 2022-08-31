1f20bad6d22e92b095bd6087284586201c4d7550
JP-Becker <jpbecker20@gmail.com>
2022-08-31 16:09:37 -0300

    a009-strings-JoaoBecker

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+const nome = prompt("Qual o seu nome")
+const comida1 = prompt("qual sua comida favorita?")
+const comida2 = prompt("qual sua segunda comida favorita?")
+const comida3 = prompt("qual sua terceira comida favorita?")
+
+console.log("Esta são as comidas favoritas de:", nome + "\n" + "1: " + comida1 + "\n2: " + comida2 + "\n3: " + comida3)
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,8 @@
 //Crie a const para a frase aqui
+const frase = ("Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS,")
+
+const fraseMaiuscula = "mas não deixe o gato sair\"".toUpperCase()
+
+const fraseTrocada = frase.replace("verde" , "rosa").replace("azul", "laranja")
+
+console.log(fraseTrocada, fraseMaiuscula)
