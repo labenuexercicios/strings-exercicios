90af668cfc6128483844897c32034597f23c4a60
CaioCmara <caio1801@gmail.com>
2022-08-31 15:49:04 -0300

    Exercícios finalizado

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,8 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const stringFinal = minhaString.replace("________","sticioso")
+
+console.log (stringFinal)
+console.log (`O número de caracteres com espaço é: ${stringFinal.length}`)
+// o trim para remover espaços
+console.log (`O número de caracteres sem espaço é: ${stringFinal.trim().length}`)
+ 
