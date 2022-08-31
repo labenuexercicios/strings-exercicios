512cf2de32fdf0ee6512059ce9d601dda1066f76
guirozmesquita@gmail.com <guilherme_mesquita@hotmail.com>
2022-08-31 12:46:10 -0400

    exercicios resolvidos 1

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+let nameUser = prompt('Olá, qual seu nome?')
+let favoriteFood1 = prompt('Qual sua comida favorita?')
+let favoriteFood2 = prompt('Qual sua segunda comida favorita?')
+let favoriteFood3 = prompt('Qual sua terceira comida favorita?')
+
+console.log(`Estas são as comidas favoritas de ${nameUser} \n- ${favoriteFood1} \n- ${favoriteFood2} \n- ${favoriteFood3}`)
