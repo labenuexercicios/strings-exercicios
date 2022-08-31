0d14bbe652bac83aa61ff2f300074b7593c883b7
VictorOgataj8 <victor.ogataj8@gmail.com>
2022-08-31 14:07:09 -0300

    feito exercico 3

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,8 +1,11 @@
-let minhaString="Eu não sou supersticioso, mas sou um pouco ________.      "
-// console.log(`antes:${minhaString.length} 
-// tirar os espacos:${minhaString.trim()} 
-// depois:${minhaString.length}
-// `)
+// let minhaString="Eu não sou supersticioso, mas sou um pouco ________.      ";
+// // console.log(`antes:${minhaString.length} 
+// // tirar os espacos:${minhaString.trim()} 
+// // depois:${minhaString.length}
+// // `)
 
-console.log(minhaString.trim(), minhaString.length)
-
+// console.log(minhaString.trim(), minhaString.length)
+let email= "   victor.ogata@homtail.com    "
+console.log(email.length)
+console.log(email.trim())
+console.log(email.length)
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,11 @@
-//Crie a const para a frase aqui
+const frase= `Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"`
+const fraserosa= frase.replace("verde", "rosa")
+const fraselaranja= fraserosa.replace("azul", "laranja")
+const frasefinal= fraselaranja.toLowerCase()
+
+
+
+
+console.log(frasefinal)
+
+
