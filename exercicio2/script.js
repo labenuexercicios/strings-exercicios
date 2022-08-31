e0f4de571d3d446d43f9bc771f8ca17f66154742
GLCalegaro <giovaluiza@gmail.com>
2022-08-31 14:55:40 -0300

    Exercícios aula 9 resolvidos

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,13 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const minhaString2 = "Eu não sou supersticioso, mas sou um pouco ________.";
+
+//*a) Remova o excesso de espaços no final da string;
+console.log(minhaString.trim("Eu não sou supersticioso, mas sou um pouco ________.      "))
+
+//b) exiba, em um console.log() a quantidade de caracteres da string, antes e depois da remoção dos espaços;
+console.log("Antes" , minhaString.length)
+console.log("Depois" , minhaString2.length)
+
+//c) Substitua os traços `________` por “sticioso”.
+const substTraço = minhaString.replaceAll("________","sticioso")
+console.log(substTraço)
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,17 @@
-//Crie a const para a frase aqui
+//*Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"
+
+//a) Crie uma `const` no seu código para guardar a frase (com aspas e tudo);
+const casaVA = ("Jorge tem uma casa verde e com portão azul, com os dizeres: BOAS VINDAS, mas não deixe o gato sair")
+console.log(casaVA)
+//b) Crie uma nova string a partir da primeira, trocando **verde** por **rosa**, e **azul** por **laranja**;
+const trocarVR = casaVA.replaceAll("verde","rosa")
+const trocarAL = trocarVR.replaceAll("azul","laranja")
+console.log(trocarAL)
+
+//c) Verifique se a nova string inclui **verde**, e se inclui **laranja**.
+console.log("A segunda frase tem as cores verde e laranja?", trocarAL.includes("verde" && "laranja"))
+//**EXTRA:** tente fazer o “mas não deixe o gato sair” ficar em maiúsculo, assim como o “BOAS VINDAS”
+let maius = ("mas não deixe o gato sair") 
+let subs = maius.toUpperCase("mas não deixe o gato sair")
+const frasefinal = trocarAL.replace("mas não deixe o gato sair" ,subs) 
+console.log(frasefinal)
