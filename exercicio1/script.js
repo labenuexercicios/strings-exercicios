f30d10f717d62df35c13ec4bc91e5f89b0b926ae
Carlos Bergson <104369215+cblouzada@users.noreply.github.com>
2022-08-31 20:24:39 -0300

    Update script.js

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -4,9 +4,9 @@
 const nome =(prompt ("Qual o nome de usuario"))
 
 // Declare 3 tipos de comida favorita dele.
-const comida1 =(prompt("Diga uma comida favoritas"))
-const comida2 =(prompt("Diga uma comida favoritas"))
-const comida3 =(prompt("Diga uma comida favoritas"))
+const comida1 =(prompt("Diga uma comida favorita"))
+const comida2 =(prompt("Diga uma comida favorita"))
+const comida3 =(prompt("Diga uma comida favorita"))
 
 // Essas são as informações que serão passadas no console.
-console.log(`Estas são as comidas favoritas de  ${nome} \n - ${comida1} \n - ${comida2} \n - ${comida3}`)
+console.log(`Essas são 3 comidas favoritas de  ${nome} \n - ${comida1} \n - ${comida2} \n - ${comida3}`)
