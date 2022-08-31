6a1e922d99fa678e2d31c1def50aaa711d7e6743
euleo11 <leonardov.silva15@gmail.com>
2022-08-31 12:50:20 -0300

    Exercicio de strings concluido

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,10 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const aux = minhaString.trim();
+const siticioso = aux.replace("________","siticioso")
+
+console.log(minhaString,`Quantidade de caracteres CONTANDO os espaços:`, minhaString.length) 
+
+
+console.log(aux,`\n`, `Quantidade de caracteres SEM contar os espaços:`, aux.length)
+
+console.log(`Substituindo os traços:`,`\n`,siticioso)
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,14 @@
 //Crie a const para a frase aqui
+
+const frase = "Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\" "
+console.log(`Frase Original:\n`,frase)
+
+const fraseAuxiliar = frase.replaceAll("verde","rosa").replace("azul","laranja")
+console.log(`\n Substituindo cores:
+${fraseAuxiliar}
+`)
+
+const gatosair = "Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\" "
+console.log(`\n Final da frase maiúsculo:
+${gatosair.toUpperCase(`mas não deixe o gato sair`)}
+`)
