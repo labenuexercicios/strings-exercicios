fba5e42e9a55b0dc058099e05a2f3ef6b71f1e23
karoline254 <karolineexecut@gmail.com>
2022-08-31 11:02:17 -0300

    Exercicio02

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,4 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+console.log(minhaString.trim())
+console.log(minhaString.length, minhaString.trim().length)
+console.log(minhaString.replace("________", "sticioso"))
