1b95979495da4315d97594d4c988020809b3ee11
JayGovinda13 <jaygovinda13@gmail.com>
2022-08-31 22:46:24 -0300

    agoraAcabou

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,9 +1,8 @@
 //Crie a const para a frase aqui
-
-const frase = "Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\" "
+let frase = "Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\" "
 console.log(frase)
 const frase2 = frase.replace("verde", "rosa",)
 frase2.replace("azul", "laranja")
 console.log(frase2)
-const fraseUp = frase.toUpperCase()
-console.log(fraseUp)
+frase = "Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, MAS NÃO DEIXA O GATO SAIR\" "
+console.log(frase)
