63ba8fdf23dd7ce3d2397a0ac83ae437b196f195
guirozmesquita@gmail.com <guilherme_mesquita@hotmail.com>
2022-09-01 07:49:35 -0400

    ajustes

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,8 +1,4 @@
 const frase = "Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\""
-console.log(frase.toLocaleUpperCase('verde'))
-
-const fraseMaiuscula = "mas não deixe o gato sair".toUpperCase();
-
-const novaFrase = frase.replaceAll('verde','rosa')
-const novaFrase2 = novaFrase.replaceAll('azul','laranja')
-console.log(novaFrase2.replaceAll("mas não deixe o gato sair", fraseMaiuscula))
+console.log(frase)
+const novaFrase = frase.replaceAll('verde', 'rosa').replaceAll('azul', 'laranja') 
+console.log(novaFrase.replaceAll("mas não deixe o gato sair", "mas não deixe o gato sair".toUpperCase()))
