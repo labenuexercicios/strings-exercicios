187dd3ea1d402e55aae494a49772eced71f39a7a
alvarezlucas <lucaasalvarez@gmail.com>
2022-09-09 12:42:35 -0500

    Exercicios resolvidos

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+// Faça um programa que pergunte ao usuário seu nome e suas três comidas favoritas. Em seguida, Imprima no console a seguinte mensagem:
+
+const nomeUsuario = prompt("Qual e o seu nome?")
+let comida1 = prompt("Qual sua comida favorita?")
+let comida2 = prompt("Digite sua segunda comida favorita")
+let comida3 = prompt("Digite sua tercecira comida favorita")
+
+console.log (`As comidas favoritas de ${nomeUsuario} sao \n ${comida1} \n ${comida2} \n ${comida3}`)
