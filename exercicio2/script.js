f8c36450296ac82b99a6aa636a6950865a631bbe
Ruan Vieira <ruan.empreenderop@gmail.com>
2022-10-29 06:57:10 -0300

    Resolução dos exercícios

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,7 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const minhaStringTrim = minhaString.trim();
+
+console.log(`Original string: ${minhaString.length}
+Trim string: ${minhaStringTrim.length}
+Replace string: ${minhaStringTrim.replace("________", "sticioso")}
+`);
