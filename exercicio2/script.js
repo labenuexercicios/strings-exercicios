60b676522d9d86c0c58396b3745f860b43bdb10c
Matheus Carvalho <matcarv07@gmail.com>
2022-11-01 20:57:23 -0300

    exercicio-concluido

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,12 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+
+//letra A
+
+
+console.log(minhaString.trim())
+console.log(minhaString.length)
+console.log(minhaString.replace("________", "sticioso"))
+
+
+
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,16 @@
 //Crie a const para a frase aqui
+
+const frase = "Jorge tem uma casa verde e com portão azul, com os dizeres: \"Boas vindas, mas não deixe o gato sair\""
+
+console.log(frase)
+
+const novaFrase = frase.replace("verde", "rosa")
+console.log(novaFrase)
+const fraseMudar = novaFrase.replace("azul", "laranja")
+console.log(fraseMudar)
+
+console.log("a frase possui a cor verde?", fraseMudar.includes("verde"))
+console.log("a frase possui a cor laranja?", fraseMudar.includes("laranja"))
+
+const fraseComParteMaiusculo = frase.replace("mas não deixe o gato sair", "mas não deixe o gato sair".toUpperCase())
+console.log(fraseComParteMaiusculo)
