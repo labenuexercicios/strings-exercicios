76dc78682605c2655ce499da5107a2ac8ebe35d2
Alberto-Faria <albertofariaa@gmail.com>
2022-11-01 19:39:54 -0300

    exercicio-resolvido

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+// exercicio 1 - solicitamdo 3 variáveis e imprimindo em um console so
+
+const nome = prompt("Qual seu nome?")
+const comida1 = prompt("Digite 3 tipos de comidas favoritas sua. \nComida 1:")
+const comida2 = prompt("Comida 2:")
+const comida3 = prompt("Comida 3")
+
+console.log(`Estas são as comidas favoritas de ${nome}: 
+\nComida 1: ${comida1} \nComida 2: ${comida2}\nComida 3: ${comida3}`)
