238967ca594ef5525cec89461b2a7184890ea2f1
Cristian Viery <viery.guimaraes01@gmail.com>
2022-11-01 20:59:03 -0300

    exercicio

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+const nomeDoUsuario = prompt("Qual seu nome")
+const comida1 = prompt("Qual sua 1° comida favorita")
+const comida2 = prompt("Qual sua 2° comida favorita")
+const comida3 = prompt("Qual sua 3° comida favorita")
+
+console.log(`Estas são as comidas favoritas de ${nomeDoUsuario}\n-${comida1}\n-${comida2}\n-${comida3}`)
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,7 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+console.log(minhaString)
+console.log(minhaString.length)
+console.log(minhaString.trim())
+console.log(minhaString.length)
+console.log(minhaString.replaceAll("________","sticioso"))
+
