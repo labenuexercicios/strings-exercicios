c803b1b2c8874baf78ea0f7dd98ad5426d72a87b
Rlopesn <nogueirarln@gmail.com>
2022-11-02 15:47:12 -0300

    srings exercicios Rafae Lopes ozemela

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+
+const nome = prompt("insira seu nome:")
+const comida1 = prompt("diga uma comida favorita")
+const comida2 = prompt("diga uma comida favorita")
+const comida3 = prompt("diga uma comida favorita")
+
+console.log(`essas sao as comidas preferidas de: ${nome}! \n1ª: ${comida1}. \n2ª: ${comida2}. \n3ª: ${comida3}.`)
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,15 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+/* a) Remova o excesso de espaços no final da string;
+
+b) exiba, em um console.log() a quantidade de caracteres da string, antes e depois da remoção dos espaços;
+
+c) Substitua os traços `________` por “sticioso”. */
+
+let novaString = minhaString.trim()
+console.log(minhaString)
+console.log(minhaString.length)
+console.log(novaString.length)
+novaString = novaString.replace("________","sticioso")
+console.log(novaString)
+
