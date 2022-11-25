861035b2c9f423fe122df401350288c956ca3438
Mariaeduardarg <msitescriativos@gmail.com>
2022-11-25 13:40:19 -0300

    strings-exercicio

---
+++ exercicio1/script.js
@@ -0,0 +1,13 @@
+let nome
+let comida1
+let comida2
+let comida3
+nome = prompt("Qual o seu nome?")
+comida1 = prompt("Qual sua comida favorita número 1?")
+comida2 = prompt("Qual sua comida favorita número 2?")
+comida3 = prompt("Qual sua comida favorita número 3?")
+
+console.log("Estas são as comidas favoritas de", nome,":")
+console.log(comida1)
+console.log(comida2)
+console.log(comida3)
