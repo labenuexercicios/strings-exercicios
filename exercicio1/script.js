836ea864e990d56f2c66222e37d36ffc16fbdd85
Kenzie <silvadesouzaanalice92@gmail.com>
2022-11-25 15:55:12 -0300

    exercicio - string

---
+++ exercicio1/script.js
@@ -0,0 +1,19 @@
+let nomeDoUsuario = prompt('Digite seu nome aqui');
+alert(`A SEGUIR DIGITE SUAS 3 COMIDAS FAVORITAS`);
+let comidaFavorita1 = prompt('Qual sua comida favorita1? ');
+let comidaFavorita2 = prompt('Qual sua comida favorita2?');
+let comidaFavorita3 = prompt('Qual sua comida favorita3?');
+
+console.log(`Estas são as comidas favoritas de ${nomeDoUsuario} \n-${comidaFavorita1} \n -${comidaFavorita2} \n -${comidaFavorita3}`);
+
+
+
+
+
+
+
+
+
+
+
+
