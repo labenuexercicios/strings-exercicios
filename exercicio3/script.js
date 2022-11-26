cb6805bb0ea8e6fab6e191e69cfdad05103b986c
Gabriel Garuthi <gabrielderu@hotmail.com>
2022-11-26 20:50:52 -0300

    New alteration

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,8 @@
-//Crie a const para a frase aqui
+const frase1 = 'Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"'
+
+let frase2 = frase1.replace("verde", "rosa")
+frase2 = frase2.replace("azul", "laranja")
+
+console.log(frase2)
+
+document.write(`<h3>${frase2}</h3>`) // Print na tela do site, <h1> abre e fecha a fraze para colocar como negrito, o 1 que ta depois de h é para definir o tamanho letra quanto maior o número menor a letra.
