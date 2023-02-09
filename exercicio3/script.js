ab31c15f4d2090f8357c5338845fda66f0460caa
JonasR30011 <jonasrocha210@gmail.com>
2023-02-09 16:56:13 -0300

    Resolução do exercício 3, faltou apenas a dica

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,9 @@
-//Crie a const para a frase aqui
+const frase = (`\`\`\`\nJorge tem uma casa verde e com o portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\" \n \`\`\``)
+const exchange = (frase.replace("verde","rosa").replace("azul","laranja"))
+const checks1 = (exchange.includes("verde"))
+const checks2 = (exchange.includes("laranja"))
+// Posso fazer o includes com 2 parâmetros
+console.log(frase)
+console.log(exchange)
+console.log(checks1)
+console.log(checks2)
