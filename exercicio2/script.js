c4167452180f1231cb6a44a6c3837d724efce42d
LivanDoVale <livanjr4@gmail.com>
2023-02-14 20:20:16 -0300

    exercicio 2 feito

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,19 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+console.log(minhaString)
+
+//A)
+const correcaoString = minhaString.trim()
+console.log(correcaoString)
+
+//B)
+ quantCaracteresAntes = minhaString.length;
+ quantCaracteresDepois = correcaoString.length
+
+ console.log(`antes tinha ${quantCaracteresAntes} caracteres`)
+ console.log(`após a correção tem ${quantCaracteresDepois} caracteres`)
+
+ //C)
+const novaMinhaString = minhaString.replace("________","sticioso")
+
+console.log(novaMinhaString);
