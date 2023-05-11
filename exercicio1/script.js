c87d4ed96c94a98282a5d2d58a61ee557c8154fe
fcroscatto <fcroscatto@gmail.com>
2023-05-11 20:51:52 -0300

    strings

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+let nome = prompt("Digite seu nome :")
+let comida1 = prompt("Digite sua principal comida favorita")
+let comida2 = prompt("Digite sua segunda comida preferida")
+let comida3 = prompt("Digite sua terceira comida preferida")
+let comidas = `Estas são as comidas favoritas de ${nome} : \n- ${comida1} \n- ${comida2} \n- ${comida3}`
+
+console.log(comidas)
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,12 @@
 //Crie a const para a frase aqui
+
+const frase = "Jorge tem uma casa verde e com portão azul, com os dizeres:\"BOAS VINDAS, mas não deixe o gato sair\""
+const primeiraParte = "Jorge tem uma casa verde e com portão azul, com os dizeres:"
+const segundaParte =  "\"BOAS VINDAS, mas não deixe o gato sair\""
+const novaFrase = frase.replace ("verde", "rosa").replace("azul" , "laranja")
+let novaFrase2 = primeiraParte + segundaParte.toUpperCase()
+
+
+console.log(novaFrase)
+console.log(novaFrase.includes("verde", "azul"))
+console.log (novaFrase2)
