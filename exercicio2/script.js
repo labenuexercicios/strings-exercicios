96a8c031086f66b80f1f0145d1d7bbbaa57fc3ab
Re-Santos <renata.prog@gmail.com>
2023-05-20 22:19:23 -0300

    Atualiza string-exercicios

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,33 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+/*
+const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      " 
+
+const retirarEspacos =(frase)=>{
+    const novaFrase=frase.trim()
+
+    console.log(novaFrase)
+    return novaFrase
+
+
+}
+const novaString=retirarEspacos(minhaString)
+
+console.log(minhaString.length)
+console.log(novaString.length)
+console.log(minhaString.replace(`________.`,`sticioso`))
+/* Também pode ser feito assim, em frase
+console.log(`A quantidade de cacteres da string antes da remoção é ${minhaString.length}`)
+console.log(`A quantidade de caracteres depois da remoção é ${novaString.length}`)*/
+//Extra (sugestão usar o replace, como?)*/
+
+function imprimeInformacoesUsuario() {
+    // implemente sua lógica aqui
+    // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
+    const nome= prompt(`Digite seu nome`)
+    const idade= prompt(`Digite sua idade`)
+    const email= prompt (`Digite seu email`)
+    const frase= (`Meu nome é ` + nome +`, `+ ` tenho ` + idade + ` anos, ` + ` e o meu email é ` + email )
+    console.log (frase)
+  
+}
+imprimeInformacoesUsuario()
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,19 @@
 //Crie a const para a frase aqui
+const fraseEspecial = () =>{
+    const string = (`Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"`)
+    
+    const trocarCores = string.replaceAll(`verde`,`rosa`).replaceAll(`azul`,`laranja`)
+
+    const temVerde = string.includes(`verde`)
+
+    const temLaranja = string.includes(`laranja`)
+
+    console.log (string)
+    console.log(trocarCores)
+    console.log(`A frase inclui a palavra verde?`, temVerde)
+    console.log(`A frase inclui a palavra laranja?`,temLaranja)
+
+    console.log (string.replace("mas não deixe o gato sair","MAS NÃO DEIXE O GATO SAIR"))
+}
+
+fraseEspecial ()
