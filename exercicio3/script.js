e9746af1a1d9575dac4bef0d7ed7d8e90fee8634
CaioKN7 <galdinocaiost@gmail.com>
2023-07-20 18:53:04 -0300

    Exercício 3 de strings finalizado

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,19 @@
 //Crie a const para a frase aqui
+
+
+function substituirCores(frase) {
+    
+    frase = frase.replace("verde", "rosa");
+    frase = frase.replace("azul", "laranja");
+    return frase;
+    
+
+}
+
+const fraseOriginal = "Jorge tem uma casa verde e com portão azul, com os dizeres: 'BOAS VINDAS, mas não deixe o gato sair'";
+const trecho = "mas não deixe o gato sair"
+const trechoMaiusculo = trecho.toUpperCase();
+const fraseComTrechoMaiusculo = fraseOriginal.replace(trecho, trechoMaiusculo)
+const fraseTrocada = substituirCores(fraseOriginal);
+console.log(fraseTrocada);
+console.log(fraseComTrechoMaiusculo);
