e8b914ac4a7b193d3b2986cb493ef791513a9ae0
CaioKN7 <galdinocaiost@gmail.com>
2023-07-20 17:01:06 -0300

    Exercícios 1 e 2 de strings

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,2 +1,10 @@
+function imprimirSemEspaco() {
+    const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+    const minhaStringSemEspacoFinal = minhaString.trim()
+    const resumo2 = `a frase possui ${minhaString.length} caracteres com os espaços. Sem os espaços a frase possui ${minhaStringSemEspacoFinal.length} caracteres`   
+    const resumo3 = minhaString.replace("________", "sticioso" )
+    console.log(resumo2)
+    console.log(resumo3)
+}
 
-
+imprimirSemEspaco()
