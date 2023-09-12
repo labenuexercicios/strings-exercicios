51b7f74e3f91abf29bb914f3d25f53717090f367
raissanfg <99144281+raissanfg@users.noreply.github.com>
2023-09-12 13:09:50 -0500

    exercicio2-done

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,2 +1,63 @@
 
 
+// function A
+// const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+// const removerEspaço = (minhaString) => {
+//     const textoLimpo = minhaString.trim();
+//     console.log(textoLimpo);
+// }
+
+// removerEspaço(minhaString)
+
+
+//--------------------------------------------
+
+
+// B 
+// ANTES DA REMOÇÃO DE ESPAÇOS
+
+// const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+// const removerEspaço = (minhaString) => {
+//     const textoLimpo = minhaString.trim();
+//     console.log(textoLimpo.length);
+
+// }
+
+// removerEspaço(minhaString)
+
+
+//--------------------------------------------
+
+
+//B
+// DEPOIS DA REMOÇÃO DE ESPAÇOS
+
+// const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+// const removerEspaço = (minhaString) => {
+//     const textoLimpo = minhaString;
+//     console.log(textoLimpo.length);
+
+// }
+
+// removerEspaço(minhaString)
+
+
+//--------------------------------------------
+
+
+//C 
+//SUBSTITUIR OS TRAÇOS PELA PALAVRA STICIOSO
+
+// const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+// const removerEspaço = (minhaString) => {
+//     const textoLimpo = minhaString;
+//     console.log(textoLimpo.replace('________', 'sticioso' ));
+
+// }
+
+// removerEspaço(minhaString)
+
