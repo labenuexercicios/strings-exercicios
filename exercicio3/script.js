0e7795a0ed026a26378e2349cd3d182c22a408d6
raissanfg <99144281+raissanfg@users.noreply.github.com>
2023-09-12 14:07:10 -0500

    exercicio3-done

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,29 @@
 //Crie a const para a frase aqui
+
+// const frase = 'Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"'
+
+// function casaDoJorge (frase) {
+//   let novaFrase = frase.replace('verde','rosa').replace('azul','laranja')
+//   console.log(novaFrase)
+// }
+
+// casaDoJorge(frase);
+
+
+
+//---------------------------------------------------
+
+
+//EXTRA 
+
+// const frase = 'Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"'
+
+// function casaDoJorge (frase) {
+//   let começoDoIndex = 74;
+//   let fimDoIndex = 100;
+//   let novaFrase = frase.substring(começoDoIndex,fimDoIndex).toUpperCase(); 
+//   let umaNovaFrase = frase.substring(0,começoDoIndex) + novaFrase + frase.substring(fimDoIndex)
+//   console.log(umaNovaFrase)
+// }
+
+// casaDoJorge(frase);
