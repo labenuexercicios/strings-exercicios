d51fe9cafd9f02810bf5c3b631a782101a33031d
nadreza <machado.andreza@hotmail.com>
2023-09-12 17:14:01 -0300

    Meus exercicios para entrega de String

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,3 +1,28 @@
 function imprimirComidasFavoritas (){
+    const nome = prompt("Digite seu nome")
+    const comida1 = prompt("Qual sua comida favorita?")
+    const comida2 = prompt("Qual sua segunda comida favorita?")
+    const comida3 = prompt ("Qual a terceira?")
 
+    console.log(`Estas são as comidas favoritas de ${nome}:
+    \n- ${comida1} \n- ${comida2}    \n- ${comida3}`)
 }
+imprimirComidasFavoritas ()
+
+
+
+/* # Exercício 1
+**Atenção:** para esse exercício você deve adicionar todas as soluções dentro da função **imprimirComidasFavoritas**.
+
+Faça um programa que pergunte ao usuário seu **nome** e suas **três comidas favoritas**.
+ Em seguida, imprima no console a seguinte mensagem:
+    
+    
+   
+    Estas são as comidas favoritas de nomeDoUsuario:
+    - Comida1
+    - Comida2
+    - Comida3
+  
+    
+Você deve usar apenas um `console.log()` para isso */
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,2 +1,38 @@
 
 
+    
+    
+
+    function stringsQtdReplace (){
+        const minhaString = 'Eu não sou supersticioso, mas sou um pouco ________   .      ';
+        const contagem1 = minhaString.length;
+        const substirui = minhaString.replace("________","sticioso");
+        const removeSpace = substirui.trim();
+        const contagem2 = removeSpace.length
+
+        console.log(`String original "${minhaString}"`);
+        console.log(`Primeira contagem de carateres da string foi ${contagem1}`);
+        console.log(`Substituindo "${removeSpace}"`);
+        console.log(`Segunda contagem foi de ${contagem2}`)
+        
+
+        
+    }
+
+    stringsQtdReplace()
+
+/* # Exercício 2
+
+Observe a string abaixo:
+
+```jsx
+    const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+```
+
+A partir dela, execute os passos abaixo:
+
+a) crie uma função que recebe **minhaString** por parâmetro e **Remova** o excesso de espaços no final da string;
+
+b) A função deve imprimir no console a **quantidade de caracteres** da string, *antes* e *depois* da remoção dos espaços;
+
+c) A função também deve **Substituir** os traços `________` pela palavra “sticioso”. */
