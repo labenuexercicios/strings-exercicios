3d0d0ff50eb83d6d90fd18eb2fa53359951c49b5
raissanfg <99144281+raissanfg@users.noreply.github.com>
2023-09-12 11:45:33 -0500

    exercicio1-done

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,3 +1,9 @@
 function imprimirComidasFavoritas (){
-
+  let nome = prompt('Qual o seu nome?')
+  let comidaFavorita1 = prompt('Qual sua  primeira comida favorita?')
+  let comidaFavorita2 = prompt('Qual sua  segunda comida favorita?')
+  let comidaFavorita3 = prompt('Qual sua  terceira comida favorita?')
+  console.log(`Estas são as comidas favoritas de ${nome}:\n-${comidaFavorita1}\n-${comidaFavorita2}\n-${comidaFavorita3}`)
 }
+
+imprimirComidasFavoritas();
